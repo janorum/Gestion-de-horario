8d99a1b85e55557660aa03b9c0b73f7c7d70e442
janorum <janofonde.ri@gmail.com>
2026-05-06 13:18:53 +0200

    añadiendo allowed host

--- gestor_horarios_django/settings.py
+++ gestor_horarios_django/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-tu-clave-aqui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # 2. Definición de Aplicaciones
 INSTALLED_APPS = [
